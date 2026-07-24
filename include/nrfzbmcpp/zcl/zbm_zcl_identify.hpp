ddaddbc5e199c3908a6e6cf3c5e5c6ea29464d2a
TheOrlangur <theorlangur@gmail.com>
2026-07-24 21:38:34 +0200

    added client clusters for identify and level control

--- include/nrfzbmcpp/zcl/zbm_zcl_identify.hpp
+++ include/nrfzbmcpp/zcl/zbm_zcl_identify.hpp
@@ -14,6 +14,29 @@ namespace zbm
             [[=attribute_a{.id = ZB_ZCL_ATTR_IDENTIFY_IDENTIFY_TIME_ID, .a = access_t::RW}]]
             zb_uint16_t identify_time{};
         };
+
+        struct 
+            [[=cluster_a{.id = ZB_ZCL_CLUSTER_ID_IDENTIFY, .role=role_t::Client, .pre_init = ZB_ZCL_CLUSTER_ID_IDENTIFY_CLIENT_ROLE_INIT}]]
+            identify_client_t
+        {
+            enum effect_id: uint8_t
+            {
+                Blink = 0x00, Breathe = 0x01, Okay=0x02, ChannelChange=0x0b, FinishEffect=0xfe, StopEffect=0xff
+            };
+            enum effect_variant: uint8_t
+            {
+                Default = 0x00
+            };
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_IDENTIFY_IDENTIFY_ID}}]]
+            [[no_unique_address]]cmd_out_t<void(uint16_t)> identify;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_IDENTIFY_IDENTIFY_QUERY_ID}}]]
+            [[no_unique_address]]cmd_out_t<void()> indentify_query;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_IDENTIFY_TRIGGER_EFFECT_ID}}]]
+            [[no_unique_address]]cmd_out_t<void(effect_id, effect_variant)> trigger_effect;
+        };
     }
 }
 #endif
--- include/nrfzbmcpp/zcl/zbm_zcl_level_ctrl.hpp
+++ include/nrfzbmcpp/zcl/zbm_zcl_level_ctrl.hpp
@@ -17,6 +17,39 @@ namespace zbm
             [[=attribute_a{.id = ZB_ZCL_ATTR_LEVEL_CONTROL_REMAINING_TIME_ID}]]
             zb_uint16_t remaining_time{};
         };
+
+        struct 
+            [[=cluster_a{.id = ZB_ZCL_CLUSTER_ID_LEVEL_CONTROL, .role = role_t::Client, .pre_init = ZB_ZCL_CLUSTER_ID_LEVEL_CONTROL_CLIENT_ROLE_INIT}]]
+            level_ctrl_client_t
+        {
+            enum fade_mode_e: uint8_t{ Up = 0, Down = 1};
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_MOVE_TO_LEVEL}}]]
+            [[no_unique_address]]cmd_out_t<void(uint8_t, uint16_t, uint8_t, uint8_t)> move_to_level;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_MOVE}}]]
+            [[no_unique_address]]cmd_out_t<void(fade_mode_e, uint8_t, uint8_t, uint8_t)> move;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_STEP}}]]
+            [[no_unique_address]]cmd_out_t<void(fade_mode_e, uint8_t, uint16_t, uint8_t, uint8_t)> step;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_STOP}}]]
+            [[no_unique_address]]cmd_out_t<void(uint8_t, uint8_t)> stop;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_MOVE_TO_LEVEL_WITH_ON_OFF}}]]
+            [[no_unique_address]]cmd_out_t<void(uint8_t, uint16_t, uint8_t, uint8_t)> move_to_level_with_on_off;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_MOVE_WITH_ON_OFF}}]]
+            [[no_unique_address]]cmd_out_t<void(fade_mode_e, uint8_t, uint8_t, uint8_t)> move_with_on_off;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_STEP_WITH_ON_OFF}}]]
+            [[no_unique_address]]cmd_out_t<void(fade_mode_e, uint8_t, uint16_t, uint8_t, uint8_t)> step_with_on_off;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_STOP_WITH_ON_OFF}}]]
+            [[no_unique_address]]cmd_out_t<void(uint8_t, uint8_t)> stop_with_on_off;
+
+            [[=cmd_out_a{{.id = ZB_ZCL_CMD_LEVEL_CONTROL_MOVE_TO_CLOSEST_FREQUENCY}}]]
+            [[no_unique_address]]cmd_out_t<void(uint16_t)> move_to_closest_freq;
+        };
     }
 }
 #endif
