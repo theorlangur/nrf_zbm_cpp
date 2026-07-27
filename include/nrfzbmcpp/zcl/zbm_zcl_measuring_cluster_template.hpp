6f0e33393a7abd75b6aa37be52468291a9418fd3
TheOrlangur <theorlangur@gmail.com>
2026-07-27 22:28:27 +0200

    fixed types of the measured value

--- include/nrfzbmcpp/zcl/zbm_zcl_measuring_cluster_template.hpp
+++ include/nrfzbmcpp/zcl/zbm_zcl_measuring_cluster_template.hpp
@@ -13,24 +13,24 @@ namespace zbm
             measure_tpl_basic_t
         {
             [[=attribute_a{.id = 0, .a = access_t::RP}]]
-            zb_uint16_t measured_value{};
+            zb_int16_t measured_value{};
         };
 
         template<uint16_t ClusterID>
         struct measure_tpl_t: measure_tpl_basic_t<ClusterID>
         {
             [[=attribute_a{.id = 1}]]
-            uint16_t min_measured_value;
+            int16_t min_measured_value;
 
             [[=attribute_a{.id = 2}]]
-            uint16_t max_measured_value;
+            int16_t max_measured_value;
         };
 
         template<uint16_t ClusterID>
         struct measure_tpl_ext_t: measure_tpl_t<ClusterID>
         {
             [[=attribute_a{.id = 3}]]
-            uint16_t tolerance;
+            int16_t tolerance;
         };
     }
 }
