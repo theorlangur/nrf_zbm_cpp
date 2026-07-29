dac664f25119155cd12d2b1a4aedc35adf9a4e98
TheOrlangur <theorlangur@gmail.com>
2026-07-29 22:22:31 +0200

    started working on implementation of poll-control cluster within the library

---
+++ include/nrfzbmcpp/zcl/zbm_zcl_poll_ctrl_impl.hpp
@@ -0,0 +1,18 @@
+#ifndef ZBM_ZCL_POLL_CTRL_IMPL_HPP_
+#define ZBM_ZCL_POLL_CTRL_IMPL_HPP_
+
+#include "../zbm.hpp"
+
+namespace zbm
+{
+    namespace zcl
+    {
+        namespace poll_ctrl_impl{
+            template<std::meta::info cluster_ref, uint8_t ep, uint8_t addHandlingDepth>
+            void poll_control_write_attr_hook_server(zb_uint8_t endpoint, zb_uint16_t attr_id, zb_uint8_t *new_value, zb_uint16_t manuf_code)
+            {
+            }
+        }
+    }
+}
+#endif
